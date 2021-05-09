c542681814dc4308537c1a23778ec05849de18b4
Sweta Kumari <swetachouhan12101995@gmail.com>
2021-05-10 00:28:35 +0530

    index_template with mappings

--- src/app/component/create-template/create-template.component.ts
+++ src/app/component/create-template/create-template.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ElkService } from '../../services/elk.service';
 import { MatDialog } from '@angular/material/dialog';
-import {TName} from '../../model/template-model';
+import {TName, Attributes, Mapper, Property, Template, Mappings} from '../../model/template-model';
 import { MappingDialerComponent } from '../mapping-dialer/mapping-dialer.component'
+
 
 @Component({
   selector: 'app-create-template',
@@ -10,13 +11,22 @@ import { MappingDialerComponent } from '../mapping-dialer/mapping-dialer.compone
   styleUrls: ['./create-template.component.css']
 })
 export class CreateTemplateComponent implements OnInit {
-  objTemplate:any={}
+  
+  //Without template
+  objTemplate: TName={"index_patterns":[]}
+  
+  //With template
+  t_property: Property={};
+  t_mappings: Mappings={"dynamic":true, "properties":this.t_property}
+  template: Template={"mappings":this.t_mappings}
+  objWithTemplate: TName={"index_patterns":[],"template":this.template}
+  
   name:string=""
-  mapping=[];
+  mapping:Array<Attributes>=[];
   Confirmation: String = "";
   index_patterns:string="";
-  basic_type:string[]=["integer","date"];
-  mapper:any;
+  index=0
+  property:Property={}
 
   constructor(private dialog:MatDialog, private elk:ElkService) {}
   
@@ -25,47 +35,68 @@ export class CreateTemplateComponent implements OnInit {
   
   openDialog() {
     const dialogRef = this.dialog.open(MappingDialerComponent, {
-      width: '750px',
-      height:'1000px',
+      width: '1298px',
+      height:'981px',
       data:this.mapping
     });
     dialogRef.afterClosed().subscribe(result=> {
+      //error can be handled here
       this.mapping=result.data
-      console.log(this.mapping)
-      console.log(this.mapping.length)
-      this.getMap()
+      this.property = this.getMap()
     });
   }
   
   createTemplate(){
-    this.objTemplate.index_patterns=this.index_patterns.split(',')
-    this.elk.createTemplate(this.objTemplate, this.name).subscribe(res => {})
+    let patterns=this.index_patterns.split(',')
+    if (patterns.length>0 && this.index>0){
+
+      this.objWithTemplate.index_patterns = patterns
+      this.t_property = this.property
+      this.t_mappings.properties = this.t_property
+      
+      this.elk.createTemplate(this.objWithTemplate, this.name).subscribe(res => {})
+    }
+    else if(patterns.length>0){
+      this.objTemplate.index_patterns = patterns
+      this.elk.createTemplate(this.objTemplate, this.name).subscribe(res => {})
+    }
     this.index_patterns="";
     this.name="";
   }
+
 
   cancel(){
     this.index_patterns="";
     this.name="";
   }
 
-  getMap(){
-    let jsonObject = {};
-    for (let i = 0; i < this.mapping.length; i++) {
-      js=JSON.stringify(this.mapping)
-      if (this.mapping[i].format==""){
-        let key = this.mapping[i].name+''
 
-        jsonObject[key]={"type": this.mapping[i].type };
-      }
-      else{
-        jsonObject[this.mapping[i].name:strin)]={
-          "type":this.mapping[i].type,
-          "format":this.mapping[i].format
-        };
+  getIndexTemplateObject(index_patterns:string[], mapping_count:number){
+    
+  }
+  
+  getMap(){
+    let PropertyObj: Property={};
+    this.index=0
+    for(let i=0;i<this.mapping.length;i++){
+      var key = this.mapping[i].name
+      
+      if (this.mapping[i].format == "" && key!= ""){  
+        let item: Mapper={"type":""};
+        item["type"] = this.mapping[i].type
+        PropertyObj[key] = item
+        this.index+=1
       }
       
+      else if (key!= ""){
+        let item:Mapper = {"type":"", "format":""}
+        item["type"] = this.mapping[i].type
+        item["format"] = this.mapping[i].format+""
+        PropertyObj[key] = item
+        this.index+=1
+      }
     }
+    return PropertyObj
   }
 
 }
